7f60b63f0e585f0eccfac9f3b37c19e09e53536b
illfate <illfate254@gmail.com>
2019-08-01 13:06:08 +0300

    Add kingpin command line parse

--- cmd/gonews/main.go
+++ cmd/gonews/main.go
@@ -5,48 +5,28 @@ import (
 	"os"
 	"time"
 
+	"github.com/alecthomas/kingpin"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/illfate/telegram-bot-go-news/pkg/bot"
 	"github.com/illfate/telegram-bot-go-news/pkg/cache"
 )
 
-const (
-	botToken            = "BOT_TOKEN"
-	lifeTimePostsEnvVar = "LIFE_TIME"
-	gopherPicFileEnvVar = "GOPHER_PIC_FILE"
-	updateTimeEnvVar    = "UPDATE_TIME"
-	scrapeUrlEnvVar     = "SCRAPE_URL"
+const botToken = "BOT_TOKEN"
+
+var (
+	lifeTime   = kingpin.Flag("lifetime", "Life time of posts").Default("24h").Duration()
+	updateTime = kingpin.Flag("updtime", "Posts update time").Default("1h").Duration()
+	scrapeUrl  = kingpin.Flag("url", "Scrape rss channel").Default("https://habr.com/ru/rss/hubs/all/").String()
 )
 
 func main() {
+	kingpin.Parse()
 	token := os.Getenv(botToken)
 	if token == "" {
 		log.Fatalf(`no %q env var`, botToken)
 	}
-	lifeTimePosts := os.Getenv(lifeTimePostsEnvVar)
-	if token == "" {
-		log.Fatalf(`no %q env var`, lifeTimePostsEnvVar)
-	}
-	lifeTime, err := time.ParseDuration(lifeTimePosts)
-	if err != nil {
-		log.Fatalf("couldn't parse time duration: %s", err)
-	}
-	gopherPicFile := os.Getenv(gopherPicFileEnvVar)
-	if gopherPicFile == "" {
-		log.Fatalf(`no %q env var`, gopherPicFileEnvVar)
-	}
-	updateTime := os.Getenv(updateTimeEnvVar)
-	if updateTime == "" {
-		log.Fatalf(`no %q env var`, updateTimeEnvVar)
-	}
-	scrapeUrl := os.Getenv(scrapeUrlEnvVar)
-	if scrapeUrl == "" {
-		log.Fatalf(`no %q env var`, scrapeUrlEnvVar)
-	}
-	updTime, err := time.ParseDuration(updateTime)
-	if err != nil {
-		log.Fatalf("couldn't parse update time: %s", err)
-	}
+
 	tgBot, err := bot.New(token)
 	if err != nil {
 		log.Fatalf("couldn't start bot: %s", err)
@@ -61,15 +41,15 @@ func main() {
 	}
 
 	c := cache.New()
-	c.ScrapePosts(scrapeUrl)
+	c.ScrapePosts(*scrapeUrl)
 
-	ticker := time.NewTicker(updTime)
+	ticker := time.NewTicker(*updateTime)
 	defer ticker.Stop()
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				c.UpdatePosts(lifeTime, scrapeUrl)
+				c.UpdatePosts(*lifeTime, *scrapeUrl)
 			}
 		}
 	}()
@@ -77,7 +57,7 @@ func main() {
 	for update := range updates {
 		switch update.Message.Command() {
 		case "next":
-			tgBot.NextCommand(update, c, gopherPicFile)
+			tgBot.NextCommand(update, c)
 		case "start":
 			tgBot.StartCommand(update)
 		}
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.12
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/alecthomas/kingpin v2.2.6+incompatible
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190717042225-c3de453c63f4 // indirect
 	github.com/antchfx/htmlquery v1.0.0 // indirect
 	github.com/antchfx/xmlquery v1.0.0 // indirect
 	github.com/antchfx/xpath v1.0.0 // indirect
